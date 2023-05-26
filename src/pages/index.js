6b92f8fa9e95dc179b6682dbcb4b56e4fbd0aa6f
Mikhael Bailly <Mikhael.bailly1@orange.fr>
2023-05-26 20:56:31 +0200

    add: Playlist name, fix UI, remove Node on swipe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Dropdown } from 'flowbite-react';
+import { Dropdown, Modal } from 'flowbite-react';
 import React, { useState, useEffect, useMemo, useRef } from 'react'
 import { getSession, useSession, signIn, signOut } from "next-auth/react"
 import dynamic from "next/dynamic"
@@ -18,6 +18,7 @@ export default function Home({connected, total, tracks}) {
     const [allTracks, setAllTracks] = useState((tracks) ? tracks : []);
     const [isPlaying, setIsPlaying] = useState(false);
     const [started, setStarted] = useState(false);
+    const [playlistName, setPlaylistName] = useState('My Custom Playlist');
 
     const { data: session } = useSession()
 
@@ -133,6 +134,8 @@ export default function Home({connected, total, tracks}) {
             arr.push(track.uri);
             setPassedTracks(arr);
 
+            childRefs[track.position].current.parentNode.remove()
+
             // On met à jour l'index pour la musique suivante
             updateCurrentIndex(track.position)
 
@@ -167,27 +170,27 @@ export default function Home({connected, total, tracks}) {
     // Create the playlist
     const createPlaylist = async () => {
         async function createPrivatePlaylist(tracksUri){
-        const { id: user_id } = await fetchWebApi('v1/me', 'GET')
+            const { id: user_id } = await fetchWebApi('v1/me', 'GET')
 
-        const playlist = await fetchWebApi(
-            `v1/users/${user_id}/playlists`, 'POST', {
-            "name": "Mika Playlist :)",
-            "description": "Playlist created with music-swipe",
-            "public": false
-        })
-        
-        await fetchWebApi(
-            `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
-            'POST'
-        );
-        
-        return playlist;
+            const playlist = await fetchWebApi(
+                `v1/users/${user_id}/playlists`, 'POST', {
+                "name": playlistName,
+                "description": "Playlist created with music-swipe",
+                "public": false
+            })
+            
+            await fetchWebApi(
+                `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
+                'POST'
+            );
+            
+            return playlist;
+        }
+        const createdPlaylist = await createPrivatePlaylist(playlistTracks);
+
+        console.log(createdPlaylist);
     }
     
-    const createdPlaylist = await createPrivatePlaylist(playlistTracks);
-    // console.log(createdPlaylist);
-    }
-
     useEffect(() => {
         setNumberOfPages((allTracks && total) ? Math.ceil(total / LIMIT) : 0);
     }, [allTracks]);
@@ -219,36 +222,35 @@ export default function Home({connected, total, tracks}) {
                             <div className='absolute z-[99999] w-100 w-full'>
                                 <div className='flex justify-between px-6 pb-6 mt-4'>
                                     <div>
+                                    
                                         <Dropdown
                                             inline
                                             label={
-                                                <PersonCircleOutline
-                                                    color={'#FFF'}
-                                                    height={'32px'}
-                                                    width={'32px'}
-                                                />
+                                                <img className="w-10 h-10 p-1 rounded-full ring-2 ring-[#C996EE]" src={(session && session.user && session.user.image) ? session.user.image : ''} alt="Spotify avatar" />
                                             }
                                         >
                                             <Dropdown.Header>
                                                 <span className="block text-sm">
-                                                    Hello {(session && session.user && session.user.name) ? session.user.name : 'stranger'}
+                                                    Hello <span className='text-[#C996EE]'>{(session && session.user && session.user.name) ? session.user.name : 'stranger'}</span>
                                                 </span>
                                             </Dropdown.Header>
                                             
-                                            <Dropdown.Item onClick={() => signOut()}>
+                                            <Dropdown.Item onClick={() => signOut()} className='text-red-600'>
                                                 Sign out
                                             </Dropdown.Item>
                                         </Dropdown>
                                     </div>
                                     <div>
-                                        <button data-modal-target="playlistModal" data-modal-toggle="playlistModal" className="flex items-center" type="button">
-                                            <MusicalNoteOutline
-                                                color={'#FFF'}
-                                                height={'32px'}
-                                                width={'32px'}
-                                            /> 
-                                            <span className='text-xs text-[#C996EE]'>{(playlistTracks.length !== 0) ?playlistTracks.length : ''}</span>
-                                        </button>
+                                        <div className="relative">
+                                            <button data-modal-target="playlistModal" data-modal-toggle="playlistModal" className="flex items-center bg-[#C996EE] rounded-full p-2" type="button">
+                                                <MusicalNoteOutline
+                                                    color={'#FFF'}
+                                                    height={'32px'}
+                                                    width={'32px'}
+                                                /> 
+                                            </button>
+                                            <span className={`${(playlistTracks.length > 0 ? '' : 'hidden')} absolute bottom-0 left-8 transform font-bold flex items-center justify-center translate-y-1/4 w-5 h-5 bg-white rounded-full text-xs text-[#C996EE]`}>{(playlistTracks.length !== 0) ?playlistTracks.length : ''}</span>
+                                        </div>
 
                                         {/* Modal */}
                                         <div id="playlistModal" tabIndex="-1" className="fixed z-[99999] top-0 left-0 right-0 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -256,7 +258,7 @@ export default function Home({connected, total, tracks}) {
                                                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                                                     <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
                                                         <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                                                            Create the playlist
+                                                            Create your playlist
                                                         </h3>
                                                         <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="playlistModal">
                                                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -265,16 +267,14 @@ export default function Home({connected, total, tracks}) {
                                                     </div>
                                                     <div className="p-6 space-y-6">
                                                         <div className='bg-[#1E073B] overflow-scroll px-10 py-5 rounded-lg h-[55vh]'>
-                                                            <h2 className='text-lg font-bold'>My custom playlist</h2>
+                                                            <h2 className='text-lg font-bold'>{playlistName}</h2>
                                                             <p className='text-sm text-[#C996EE]'>Playlist created with music-swipe</p>
             
                                                             <div className='mt-5'>
                                                                 <ul className='flex flex-col gap-2'>
                                                                     {
                                                                         (playlistTracksFull.length == 0) ? 
-                                                                            <>
-                                                                            Empty playlist
-                                                                            </>
+                                                                            <span className='italic text-xs text-gray-400'>It&apos;s too quiet I don&apos;t like it much</span>
                                                                         :
                                                                             playlistTracksFull.map((item, index) => (
                                                                                 <li className='flex items-center' key={index + 1}>
@@ -291,9 +291,8 @@ export default function Home({connected, total, tracks}) {
                                                         </div>
                                                     </div>
                                                     <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                                        {playlistTracks.length > 0 ? <>
-                                                            <button data-modal-hide="playlistModal" onClick={() => createPlaylist()} type="button" className="text-white bg-[#9658c2] hover:bg-[#C996EE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Process</button>
-                                                        </> : ''}
+                                                        <input onChange={(e) => setPlaylistName(e.target.value)} type="text" className={`${(playlistTracks.length > 0 ? '' : 'hidden')} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5`} placeholder="My playlist name"/>
+                                                        <button data-modal-hide="playlistModal" onClick={() => createPlaylist()} type="button" className={`${(playlistTracks.length > 0 ? '' : 'hidden')} text-white bg-[#9658c2] hover:bg-[#C996EE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Create</button>
                                                         <button data-modal-hide="playlistModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
                                                     </div>
                                                 </div>
@@ -309,8 +308,8 @@ export default function Home({connected, total, tracks}) {
                                     <div className='absolute w-full h-full flex items-center justify-center'>
                                         {
                                             currentPage < numberOfPages ? <>
-                                                <button onClick={() => loadMore()} className='text-[#C996EE] underline mr-2'>Load more</button> (Page <span className='text-[#C996EE] mx-2'>{currentPage}</span> sur <span className='text-[#C996EE] ml-2'>{Math.ceil(total / LIMIT) }</span>)
-                                            </> : <> Derniere page</>
+                                                <button onClick={() => loadMore()} className='text-[#C996EE] underline mr-2'>Load more</button> (Page <span className='text-[#C996EE] mx-2'>{currentPage}</span> on <span className='text-[#C996EE] ml-2'>{Math.ceil(total / LIMIT) }</span>)
+                                            </> : <> You&apos;ve reached the end</>
                                         }
                                     </div>
         
@@ -328,15 +327,15 @@ export default function Home({connected, total, tracks}) {
                                                 <div ref={childRefs[item.position]} className={`w-full h-full relative overflow-hidden bg-cover bg-center`} style={{ backgroundImage: `url(${item.cover})` }}>
                                                     <div className="absolute bottom-0 w-full flex justify-between px-6 pb-6 mt-6">
                                                         <div className="">
-                                                            <span className="block">{item.name}</span>
-                                                            <span className="block font-semibold text-xl">{item.artists}</span>
+                                                            <span className="block font-semibold text-[#C996EE] text-3xl">{item.name}</span>
+                                                            <span className="block italic text-lg font-medium">{item.artists}</span>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </TinderCard>
                                         ))
                                     :
-                                        <>Aucune musique </>
+                                        <>No favorite music</>
                                     }
         
                                     {/* Play button */}
