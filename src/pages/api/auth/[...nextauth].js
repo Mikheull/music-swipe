9e4d68cf0fdf12ceeffb790a5dfe456a489bc644
Mikhael Bailly <Mikhael.bailly1@orange.fr>
2023-05-30 02:05:29 +0200

    Wip: Deezer implementation

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,18 +9,68 @@ export default NextAuth({
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
       authorization: "https://accounts.spotify.com/authorize?scope=user-library-read,playlist-modify-private,playlist-modify-public"
     }),
+
+    {
+      id: "deezer",
+      name: "Deezer",
+      type: "oauth",
+      authorization: {
+        url: "https://connect.deezer.com/oauth/auth.php",
+        params: { perms: "basic_access,manage_community,manage_library,offline_access" },
+      },
+      token: {
+        async request({ provider, params }) {
+          const res = await fetch(
+            `https://connect.deezer.com/oauth/access_token.php?${new URLSearchParams({
+              app_id: provider.clientId,
+              secret: provider.clientSecret,
+              code: params.code,
+            })}`
+          )
+          const data = new URLSearchParams(await res.text())
+    
+          return {
+            tokens: {
+              access_token: data.get("access_token"),
+              expires_at: Math.ceil(Date.now() / 1000 + +data.get("expires")),
+            },
+          }
+        },
+      },
+      clientId: process.env.DEEZER_ID,
+      clientSecret: process.env.DEEZER_SECRET,
+      userinfo: {
+        url: "https://api.deezer.com/user/me",
+        request({ tokens, client }) {
+          const { access_token } = tokens
+          return client.userinfo(access_token, { params: { access_token } })
+        },
+      },
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
+    },
   ],
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
+        token.provider = account.provider
+        token.accountId = account.providerAccountId
       }
       return token
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
+      session.provider = token.provider
+      session.accountId = token.accountId
       return session
     }
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import { Dropdown } from 'flowbite-react';
 import React, { useState, useEffect, useMemo, useRef } from 'react'
 import { getSession, useSession, signIn, signOut } from "next-auth/react"
 import dynamic from "next/dynamic"
-import { PlayCircleOutline, PauseCircleOutline, MusicalNoteOutline, TrashOutline } from 'react-ionicons'
+import { PlayCircleOutline, PauseCircleOutline, MusicalNoteOutline, TrashOutline, AlbumsOutline } from 'react-ionicons'
 
 const TinderCard = dynamic(() => import('../libs/react-tinder-card.js'), {
   ssr: false
 });
 const LIMIT = 50;
 
-export default function Home({connected, total, tracks}) {
+export default function Home({connected, total, tracks, provider}) {
     const [numberOfPages, setNumberOfPages] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
     const [playlistTracks, setPlaylistTracks] = useState([]);
@@ -43,6 +43,13 @@ export default function Home({connected, total, tracks}) {
             headers: {
                 Authorization: `Bearer ${session.accessToken}`,
             },
+            method,
+            body:JSON.stringify(body)
+        });
+        return await res.json();
+    }
+    async function fetchDeezerWebApi(endpoint, method, body) {
+        const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/${endpoint}`, {
             method,
             body:JSON.stringify(body)
         });
@@ -168,24 +175,48 @@ export default function Home({connected, total, tracks}) {
     // Create the playlist
     const createPlaylist = async () => {
         async function createPrivatePlaylist(tracksUri){
-            const { id: user_id } = await fetchWebApi('v1/me', 'GET')
+            if(provider == 'spotify'){
+                const playlist = await fetchWebApi(
+                    `v1/users/${session.accountId}/playlists`, 'POST', {
+                    "name": playlistName,
+                    "description": "Playlist created with music-swipe",
+                    "public": false
+                })
+                
+                await fetchWebApi(
+                    `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
+                    'POST'
+                );
+                
+                return playlist;
+            } else if(provider == 'deezer'){
+                console.log('create from deezer', session);
+        // http://api.deezer.com/playlist/<playlist_id>/tracks?access_token=<access_token>&request_method=post&songs=<track_id>
 
-            const playlist = await fetchWebApi(
-                `v1/users/${user_id}/playlists`, 'POST', {
-                "name": playlistName,
-                "description": "Playlist created with music-swipe",
-                "public": false
-            })
-            
-            await fetchWebApi(
-                `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
-                'POST'
-            );
-            
-            return playlist;
+        
+        console.log(session.accessToken);
+                const playlist = await fetchDeezerWebApi(
+                    `user/${session.accountId}/playlists?access_token=${session.accessToken}`, 'POST', {
+                    "title": playlistName,
+                    "description": "Playlist created with music-swipe",
+                    "public": false
+                })
+                
+                const add_songs = await fetchDeezerWebApi(
+                    `playlist/${playlist.id}/tracks?access_token=${session.accessToken}`, 'POST', {
+                    "songs": tracksUri.join(','),
+                    "description": "Playlist created with music-swipe",
+                    "public": false
+                })
+                console.log(add_songs);
+                
+                return playlist;
+            }
+
+            return false
         }
         const createdPlaylist = await createPrivatePlaylist(playlistTracks.map( e => e.uri ));
-        // console.log(createdPlaylist);
+        console.log(createdPlaylist);
     }
     
     useEffect(() => {
@@ -198,8 +229,26 @@ export default function Home({connected, total, tracks}) {
     if(connected == false) {
         return (
           <>
-            <div className='absolute w-screen h-[calc(100dvh)] flex items-center justify-center bg-white'>
-                <button onClick={() => signIn('spotify')} type="button" className="text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+            <div className='absolute w-screen h-[calc(100dvh)] bg-primary'>
+                <div className="mx-auto max-w-lg bg-[#1E073B] h-full">
+                    <div className='relative w-full h-[calc(100dvh)] overflow-hidden flex flex-col gap-2 items-center justify-center'>
+                        <AlbumsOutline
+                            color={'#FFF'}
+                            height={'48px'}
+                            width={'48px'}
+                        /> 
+
+                        <h1 className='text-center block font-semibold text-white text-3xl'>Create your ideal<br/> playlist</h1>
+                        <button onClick={() => signIn('spotify')} type="button" className="flex gap-2 items-center justify-center mt-6 text-white bg-spotify hover:bg-spotify-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            <svg fill='#FFF' width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z"/></svg>
+                            Sign in with Spotify
+                        </button>
+                        <button onClick={() => signIn('deezer')} type="button" className="flex gap-2 items-center justify-center mt-6 text-white bg-deezer hover:bg-deezer-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            <svg fill='#FFF' width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z"/></svg>
+                            Sign in with Deezer
+                        </button>
+                    </div>
+                </div>
             </div>
           </>
         )
@@ -254,7 +303,7 @@ export default function Home({connected, total, tracks}) {
                                                 <div className="relative bg-white rounded-lg shadow">
                                                     <div className="flex items-center justify-between p-5 border-b rounded-t">
                                                         <h3 className="text-xl font-medium text-gray-900">
-                                                            Create your playlist
+                                                            Create your playlist from {provider == 'spotify' ? 'Spotify' : 'Deezer'}
                                                         </h3>
                                                         <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="playlistModal">
                                                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -315,11 +364,11 @@ export default function Home({connected, total, tracks}) {
                                         {
                                             currentPage < numberOfPages ? <>
                                                 <div role="status">
-                                                    <svg aria-hidden="true" class="w-8 h-8 mr-2 animate-spin text-primary-700 fill-primary-700" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <svg aria-hidden="true" className="w-8 h-8 mr-2 animate-spin text-primary-700 fill-primary-700" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                                                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                                                     </svg>
-                                                    <span class="sr-only">Loading...</span>
+                                                    <span className="sr-only">Loading...</span>
                                                 </div>
                                             </> : <p className='text-primary-700'> You&apos;ve reached the end</p>
                                         }
@@ -406,34 +455,110 @@ export async function getServerSideProps(context) {
     }
 
     try {
-        // Get the 50 last recent saved tracks
-        async function getSavedTracks(url){
-            return await fetchWebApi(url, 'GET');
-        }
-        const savedTracks = await getSavedTracks(`https://api.spotify.com/v1/me/tracks?offset=0&limit=${LIMIT}`);
-        if(savedTracks.error){
-            return {
-                props : {
-                    connected: false,
-                }
+        if(session.provider){
+            switch (session.provider) {
+                case 'deezer':
+                    // Get the 50 last recent saved tracks
+                    async function getDeezerPlaylist(url){
+                        return await fetchWebApi(url, 'GET');
+                    }
+                    const deezer_allPlaylists = await getDeezerPlaylist(`https://api.deezer.com/user/${session.accountId}/playlists`);
+                    if(deezer_allPlaylists.error){
+                        return {
+                            props : {
+                                connected: false,
+                            }
+                        }
+                    }
+
+                    const loved_playlist = deezer_allPlaylists.data.find((el) => el.is_loved_track == true);
+                    if(!loved_playlist){
+                        return {
+                            props : {
+                                tracks : [],
+                                total: 0,
+                                connected: true,
+                                provider: 'deezer'
+                            }
+                        }
+                    }
+
+                    async function getDeezerLovedTracks(url){
+                        return await fetchWebApi(url, 'GET');
+                    }
+                    const loved_tracks = await getDeezerLovedTracks(`https://api.deezer.com/playlist/${loved_playlist.id}/tracks`);
+                    if(loved_tracks.error){
+                        return {
+                            props : {
+                                tracks : [],
+                                total: 0,
+                                connected: true,
+                                provider: 'deezer'
+                            }
+                        }
+                    }
+
+                    return {
+                        props : {
+                            tracks : loved_tracks.data.map((track, index) => ({
+                                position: index + 1,
+                                id: track.id,
+                                uri: track.id,
+                                name: track.title,
+                                preview_url: track.preview,
+                                artists: Array.isArray(track.artist) ? track.artist.map(artist => artist.name).join(', ') : track.artist.name,
+                                cover: track.album.cover_big,
+                            })),
+                            total: loved_playlist.nb_tracks,
+                            connected: true,
+                            provider: 'deezer'
+                        }
+                    }
+                    break;
+            
+                case 'spotify':
+                    // Get the 50 last recent saved tracks
+                    async function getSpotifySavedTracks(url){
+                        return await fetchWebApi(url, 'GET');
+                    }
+                    const spotify_savedTracks = await getSpotifySavedTracks(`https://api.spotify.com/v1/me/tracks?offset=0&limit=${LIMIT}`);
+                    if(spotify_savedTracks.error){
+                        return {
+                            props : {
+                                connected: false,
+                            }
+                        }
+                    }
+
+                    return {
+                        props : {
+                            tracks : spotify_savedTracks.items.map(({ track }, index) => ({
+                                position: index + 1,
+                                id: track.id,
+                                uri: track.uri,
+                                name: track.name,
+                                preview_url: track.preview_url,
+                                artists: track.artists.map(artist => artist.name).join(', '),
+                                cover: track.album.images[0].url,
+                            })),
+                            total: spotify_savedTracks.total,
+                            connected: true,
+                            provider: 'spotify'
+                        }
+                    }
+                    break;
+        
+                default:
+                    break;
             }
         }
 
         return {
             props : {
-                tracks : savedTracks.items.map(({ track }, index) => ({
-                    position: index + 1,
-                    id: track.id,
-                    uri: track.uri,
-                    name: track.name,
-                    preview_url: track.preview_url,
-                    artists: track.artists.map(artist => artist.name).join(', '),
-                    cover: track.album.images[0].url,
-                })),
-                total: savedTracks.total,
-                connected: true,
+                connected: false,
             }
         }
+        
     } catch(error) {
         return {
             props : {
